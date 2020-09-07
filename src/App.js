790d4f67f46944ebf2f0afc953dc3aa5de4bdf38
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2020-09-07 12:51:31 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const openMenu = () => {
+  document.querySelector(".sidebar").classList.add("open");
+};
+const closeMenu = () => {
+  document.querySelector(".sidebar").classList.remove("open");
+};
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
